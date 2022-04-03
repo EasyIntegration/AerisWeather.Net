88e07e2e9bb8df31be40d178580b31c00d59211a
kullenwilliams <kullenwilliams.13@gmail.com>
2022-04-03 13:53:37 -0500

    :white-check: add test

---
+++ AerisWeather.Net.Tests.Integration/AirQualityIntegrationTests/AirQualityNowIntegrationTests.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Threading.Tasks;
+using AerisWeather.Net.Models.Exceptions;
+using Xunit;
+
+namespace AerisWeather.Net.Tests.Integration.AirQualityIntegrationTests
+{
+    public class AirQualityNowIntegrationTests : BaseAirQualityIntegrationTest
+    {
+
+
+
+        [Fact]
+        public async Task  Now_CityState_LocationNotFound()
+        {
+            await Assert.ThrowsAsync<LocationNotFoundException>(() => this.airQuality.NowAsync("birmingham2", "12"));
+        }
+
+        [Theory]
+        [InlineData("", "")]
+        [InlineData(" ", " ")]
+        [InlineData("123", "12")]
+        [InlineData("notfound", "az")]
+        public async Task Now_CityState_LocationNotFound_Theory(string param1, string param2)
+        {
+            await Assert.ThrowsAsync<LocationNotFoundException>(() => this.airQuality.NowAsync(param1, param2));
+        }
+
+        [Theory]
+        [InlineData("birmingham", "al")]
+        [InlineData("atlanta", "ga")]
+        [InlineData("nashville", "tn")]
+        [InlineData("austin", "tx")]
+        [InlineData("alamogordo", "nm")]
+        [InlineData("mesa", "az")]
+        [InlineData("renton", "wa")]
+        [InlineData("Prudhoe Bay","AK")]
+        public async Task Now_CityState_Theory(string param1, string param2)
+        {
+            var result = await this.airQuality.NowAsync(param1, param2);
+
+            Assert.NotNull(result);
+        }
+
+
+        [Fact]
+        public async Task Now_ZIP_LocationNotFound()
+        {
+            await Assert.ThrowsAsync<LocationNotFoundException>(() => this.airQuality.NowAsync("zzzzzz"));
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(" ")]
+        [InlineData("123")]
+        [InlineData("notfound")]
+        public async Task Now_ZIP_LocationNotFound_Theory(string param1)
+        {
+            await Assert.ThrowsAsync<LocationNotFoundException>(() => this.airQuality.NowAsync(param1));
+        }
+
+        [Theory]
+        [InlineData("35040")]
+        [InlineData("99734")]
+        [InlineData("S7M 0Z9")]
+        [InlineData("33142")]
+        public async Task Now_ZIP_Theory(string param1)
+        {
+            var result = await this.airQuality.NowAsync(param1);
+
+            Assert.NotNull(result);
+        }
+    }
+}
---
+++ AerisWeather.Net.Tests.Integration/AirQualityIntegrationTests/BaseAirQualityIntegrationTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using AerisWeather.Net.Clients;
+using K23.Aeris.NetCore.Tests.Integration;
+using Xunit;
+
+namespace AerisWeather.Net.Tests.Integration.AirQualityIntegrationTests
+{
+    public class BaseAirQualityIntegrationTest : BaseAerisIntegrationTests
+    {
+        public IAirQuality airQuality;
+
+        public BaseAirQualityIntegrationTest()
+        {
+            this.airQuality = new AirQuality(new AerisClient());
+        }
+
+        
+    }
+}
